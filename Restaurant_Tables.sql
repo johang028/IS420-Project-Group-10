fcf66a298fede2c98bb2467967afcf84c41d7478
SungJi-umbc <117539247+SungJi-umbc@users.noreply.github.com>
2023-03-15 16:27:49 -0400

    Update Restaurant_Tables.sql

--- Restaurant_Tables.sql
+++ Restaurant_Tables.sql
@@ -49,7 +49,7 @@ CREATE sequence seq_cid
 
 -- T01.3: insert data to customer
 INSERT INTO customer 
-	VALUES (1, 'Wendi Robles', '202 W. Shore Ave', 'OH', 43612, 'rattha@problemstory.us', 0);
+	VALUES (seq_cid.nextval, 'Wendi Robles', '202 W. Shore Ave', 'OH', 43612, 'rattha@problemstory.us', 0);
 INSERT INTO customer 
 	VALUES (seq_cid.nextval, 'Darnell House', '7765 Taklin Hill Dr.', 'CA', 95301, 'fxllipek@mtcxmail.com', 350.00);
 INSERT INTO customer
@@ -73,13 +73,14 @@ CREATE TABLE discount (
 
 -- T02.2: create autosequence for DID
 CREATE sequence discount_seq
-	minvalue 1
-	increment by 1;
+	minvalue 0
+	start with 1
+	increment by 1
 	cache 50;
 
 -- T02.3: insert date to discount
 INSERT INTO discount (did, discount_description, discount_type) 
-	VALUES (1, 'Free delivery', 1);
+	VALUES (discount_seq.nextval, 'Free delivery', 1);
 INSERT INTO discount (did, discount_description, discount_type) 
 	VALUES (discount_seq.nextval, '10% off total charge', 2);
 INSERT INTO discount (did, discount_description, discount_type) 
@@ -104,7 +105,7 @@ CREATE sequence seq_stid
 
 -- T03.3: insert data to saletax
 INSERT INTO salestax
-	VALUES (1, 'AL', '0.0400');
+	VALUES (seq_stid.nextval, 'AL', '0.0400');
 INSERT INTO salestax
 	VALUES (seq_stid.nextval, 'AK', 0.0000);
 INSERT INTO salestax
@@ -115,6 +116,8 @@ INSERT INTO salestax
 	VALUES (seq_stid.nextval, 'MD', 0.0600);
 INSERT INTO salestax
 	VALUES (seq_stid.nextval, 'NY', 0.0450);
+INSERT INTO salestax
+	VALUES (seq_stid.nextval, 'PA', 0.0600);
 INSERT INTO salestax
 	VALUES (seq_stid.nextval, 'VA', 0.0530);
 INSERT INTO salestax
@@ -166,7 +169,7 @@ CREATE sequence res_seq
 
 -- T05.3: insert data to restaurant
 INSERT INTO restaurant 
-	VALUES (1, 'Bobs Pizza', '4434568171', 'open', '123 street', '46172', 'MD', null, null);
+	VALUES (res_seq.nextval, 'Bobs Pizza', '4434568171', 'open', '123 street', '46172', 'MD', null, null);
 INSERT INTO restaurant 
 	VALUES(res_seq.nextval, 'Timmy Tams', '4637263758', 'closed', '123 ave', '74582', 'NY', null, null);
 INSERT INTO restaurant 
@@ -196,7 +199,7 @@ CREATE sequence seq_rvid
 
 -- T06.3: insert data to review
 INSERT INTO review
-	VALUES (1, 3, 3, date '2023-02-01', 1.0, 'Terrible');
+	VALUES (seq_rvid.nextval, 3, 3, date '2023-02-01', 1.0, 'Terrible');
 INSERT INTO review
 	VALUES (seq_rvid.nextval, 1, 1, date '2023-02-02', 5.0, 'very good');
 INSERT INTO review
@@ -225,7 +228,7 @@ CREATE sequence dish_seq
 
 -- T07.3: insert data to dish
 INSERT INTO dish 
-	VALUES (1, 1, 'Cheese Pizza', 9.00);
+	VALUES (dish_seq.nextval, 1, 'Cheese Pizza', 9.00);
 INSERT INTO dish 
 	VALUES (dish_seq.nextval, 1, 'Pineapple Pizza', 10.00);
 INSERT INTO dish 
@@ -253,7 +256,7 @@ CREATE sequence cat_seq
 
 -- T08.3: insert data to category
 INSERT INTO category 
-	VALUES (1, 'Italian');
+	VALUES (cat_seq.nextval, 'Italian');
 INSERT INTO category 
 	VALUES (cat_seq.nextval, 'Fast Food');
 INSERT INTO category 
@@ -301,14 +304,15 @@ CREATE sequence seq_cart
 
 -- T10.3: insert data to cart
 INSERT INTO cart
-	VALUES (1, 1, 1);
+	VALUES (seq_cart.nextval, 1, 1);
 INSERT INTO cart
 	VALUES (seq_cart.nextval, 1, 2);
 INSERT INTO cart
 	VALUES (seq_cart.nextval, 2, 2);
 
 -- T11 Cart Dish
--- T11.1: create cart_dish tablecreate table cart_dish(
+-- T11.1: create cart_dish table
+CREATE TABLE cart_dish(
 	cartid int,
 	resid int,
 	dishid int,
@@ -322,10 +326,8 @@ INSERT INTO cart
 -- T11.2: insert data to cart_dish
 INSERT INTO cart_dish
 	VALUES (1, 1, 1, 10);
-
 INSERT INTO cart_dish
 	VALUES (2, 2, 1, 5);
-
 INSERT INTO cart_dish
 	VALUES (3, 2, 2, 15);
 
@@ -358,11 +360,9 @@ CREATE sequence seq_itemOrder
 	cache 50;
 
 INSERT INTO itemOrder
-	VALUES (1, 1, 1, timestamp '2023-3-12 09:00:30.75', timestamp '2023-3-12 09:30:30.75', timestamp '2023-3-12 09:20:30.75', 1, 1, 20.20, 1);
-
+	VALUES (seq_itemOrder.nextval, 1, 1, timestamp '2023-3-12 09:00:30.75', timestamp '2023-3-12 09:30:30.75', timestamp '2023-3-12 09:20:30.75', 1, 1, 20.20, 1);
 INSERT INTO itemOrder
 	VALUES (seq_itemOrder.nextval, 2, 2, timestamp '2023-3-12 10:00:30.75', null, timestamp '2023-3-12 10:15:30.75', 1, 2, 50.25, 2);
-
 INSERT INTO itemOrder
 	VALUES (seq_itemOrder.nextval, 3, 2, timestamp '2023-3-12 12:15:30.75', null, null, 3, 2, 0, 2);
 
@@ -409,7 +409,7 @@ CREATE sequence pay_seq
 
 -- T14.3: insert data to payment
 INSERT INTO payment 
-	VALUES (1,1,1,76.88,'Credit/Debit Card');
+	VALUES (pay_seq.nextval,1,1,76.88,'Credit/Debit Card');
 INSERT INTO payment 
 	VALUES (pay_seq.nextval,2,2,64.12,'Credit/Debit Card');
 INSERT INTO payment 
@@ -436,8 +436,8 @@ CREATE sequence mes_seq
 
 -- T15.3: insert data to message
 INSERT INTO message 
-	VALUES (1,1, date '2023-1-28', 'Receipt concerns with previous visit to the restaurant');
+	VALUES (mes_seq.nextval, 1, date '2023-1-28', 'Receipt concerns with previous visit to the restaurant');
 INSERT INTO message 
-	VALUES (mes_seq.nextval,2, date '2023-1-30', 'A personal message thanking the head chef.');
+	VALUES (mes_seq.nextval, 2, date '2023-1-30', 'A personal message thanking the head chef.');
 INSERT INTO message 
 	VALUES (mes_seq.nextval,3, date '2023-2-15', 'Is this restaurant open on weekends?');
